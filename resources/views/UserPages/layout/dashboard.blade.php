9b3abb5fdedfd6837fe2dad08f438012b1078019
Duta Nugraha <duta.nugraha82@gmail.com>
2024-04-29 20:06:57 +0700

    perbaikan tampilan berita dan foto lurah

--- resources/views/UserPages/layout/dashboard.blade.php
+++ resources/views/UserPages/layout/dashboard.blade.php
@@ -21,30 +21,30 @@
             <div class="col-md-4 d-flex" data-aos="flip-left" data-aos-delay="30"
             data-aos-duration="1500">
               <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-indicators">
+                {{-- <div class="carousel-indicators">
                   <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                   <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
                   <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                </div>
+                </div> --}}
                 <div class="carousel-inner">
                   <div class="carousel-item active">
+                    <img src="{{ asset('img/lurah.jpg') }}" class="d-block w-100 rounded" alt="gambar">
+                  </div>
+                  {{-- <div class="carousel-item">
                     <img src="{{ asset('img/lurah.jpeg') }}" class="d-block w-100 rounded" alt="...">
                   </div>
                   <div class="carousel-item">
                     <img src="{{ asset('img/lurah.jpeg') }}" class="d-block w-100 rounded" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                    <img src="{{ asset('img/lurah.jpeg') }}" class="d-block w-100 rounded" alt="...">
-                  </div>
+                  </div> --}}
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                {{-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
                   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                   <span class="visually-hidden">Previous</span>
                 </button>
                 <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
                   <span class="carousel-control-next-icon" aria-hidden="true"></span>
                   <span class="visually-hidden">Next</span>
-                </button>
+                </button> --}}
               </div>
             </div>
             <div class="col-md-8 mt-3" data-aos="flip-right" data-aos-delay="30"
@@ -76,13 +76,13 @@
           <div class="container">
             <div class="row mb-3">
               @foreach ($berita as $item)
-              <div class="col-md-4">
-                <div class="card shadow-lg mx-auto" style="width: 18rem;">
-                  <img src="{{ asset('storage'.'/'.$item->gambar) }}" class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <p class="card-title text-dark">{{ Str::upper($item->judul) }}</p>
-                    <a href="/berita/{{ $item->slug }}" class="btn bg-gradient-info  d-block">Baca Selengkapnya</a>
+              <div class="col-md-4 my-4">
+                <div class="card shadow-lg mx-auto" style="width: 18rem; height: 23rem;">
+                  <img src="{{ asset('storage'.'/'.$item->gambar) }}" class="card-img-top" style="max-height: 200px; object-fit: contain" alt="foto_berita">
+                  <div class="card-body overflow-hidden" style="height: 80px">
+                    <p class="card-title text-dark text-break">{{ Str::upper($item->judul) }}</p>
                   </div>
+                  <a href="/berita/{{ $item->slug }}" class="btn bg-gradient-info d-block mx-auto my-4">Baca Selengkapnya</a>
                 </div>
               </div>
               @endforeach
